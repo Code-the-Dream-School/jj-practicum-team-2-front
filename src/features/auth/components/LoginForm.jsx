e4dc1a103330ce0ef2ba4a67b14df644d3e9334f
leg-aman <126026416+leg-aman@users.noreply.github.com>
2025-08-22 19:12:04 -0400

    login validation completed.

--- src/features/auth/components/LoginForm.jsx
+++ src/features/auth/components/LoginForm.jsx
@@ -1,18 +1,71 @@
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 const LoginForm = () => {
+    const initialLoginData = { email: "", password: "" }
+    const [loginData, setLoginData] = useState(initialLoginData)
+    const [loginDataErrors, setLoginDataErrors] = useState({})
+    const [isSubmit, setIsSubmit] = useState(false)
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setLoginData({ ...loginData, [name]: value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setLoginDataErrors(validate(loginData))
+        setIsSubmit(true)
+    }
+
+    useEffect(() => {
+        console.log(loginDataErrors)
+        if (Object.keys(loginDataErrors).length === 0 && isSubmit) {
+            console.log(loginData)
+        }
+    }, [loginDataErrors])
+
+    const validate = (val) => {
+        const errors = {}
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i
+        if (!val.email) {
+            errors.email = "Email is required!"
+        } else if (!regex.test(val.email)) {
+            errors.email = "This is not a valid email format"
+        }
+        if (!val.password) {
+            errors.password = "Password is required!"
+        } else if (val.password.length < 4) {
+            errors.password = "Password must be more than 4 characters"
+        } else if (val.password.length > 10) {
+            errors.password = "password cannot exceed more than 10 characters"
+        }
+        return errors
+    }
+
     return (
         <div className="w-full lg:w-1/2 flex flex-col justify-center items-center p-8">
             <h3 className="text-2xl font-semibold mb-2">MentorHub</h3>
             <p className="mb-6 text-gray-600">Welcome back! Please enter your details.</p>
-            <form>
+             <div className="container">
+                        {Object.keys(loginDataErrors).length === 0 && isSubmit ? (
+                            <div className="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+                                <span class="font-medium">Login Successful!</span>
+                            </div>
+                        ): (
+                            <pre>{JSON.stringify(loginData, undefined, 2)}</pre>
+                        )}
+                    </div>
+            <form onSubmit={handleSubmit}>
                 <div className="w-full max-w-md">
                     <div className="mb-4">
                         <label htmlFor="email" className="block mb-1 text-left text-gray-700">Email</label>
-                        <input type="text" name="email" className="w-full p-2 border border-gray-300 rounded" placeholder="Enter your email" required />
+                        <input type="text" name="email" value={loginData.email} onChange={handleChange} className="w-full p-2 border border-gray-300 rounded" placeholder="Enter your email" />
+                    <p className='text-sm text-red-800'>{loginDataErrors.email}</p>
                     </div>
                     <div className="mb-6">
                         <label htmlFor="password" className="block mb-1 text-left text-gray-700">Password</label>
-                        <input type="password" name="password" className="w-full p-2 border border-gray-300 rounded" placeholder="Enter your password" required />
+                        <input type="password" name="password" value={loginData.password} onChange={handleChange} className="w-full p-2 border border-gray-300 rounded" placeholder="Enter your password"  />
+                    <p className='text-sm text-red-800'>{loginDataErrors.password}</p>
                     </div>
                     <div className="mb-4">
                         <button className="w-3xs bg-blue-900 text-white py-2 rounded" style={{
@@ -21,7 +74,7 @@ const LoginForm = () => {
                     </div>
                     <div>
                         <p className="text-gray-600">
-                            Don't have an account? <Link to='/SignUp' className="text-red-700 font-semibold">Sign up</Link>
+                            Don't have an account? <Link to='/SignUp' className="text-red-700">Sign up</Link>
                         </p>
                     </div>
                 </div>
--- src/features/auth/components/SignupForm.jsx
+++ src/features/auth/components/SignupForm.jsx
@@ -6,6 +6,17 @@ const SignupForm = () => {
             <p className="mb-6 text-gray-600">Welcome! Please enter your details.</p>
             <form>
                 <div className="w-full max-w-md">
+
+                    <div class="grid md:grid-cols-2 md:gap-6">
+                        <div className="mb-4">
+                            <label htmlFor="fname" className="block mb-1 text-left text-gray-700">First Name</label>
+                            <input type="text" name="fname" className="w-full p-2 border border-gray-300 rounded" placeholder="John" required />
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="lname" className="block mb-1 text-left text-gray-700">Last Name</label>
+                            <input type="text" name="lname" className="w-full p-2 border border-gray-300 rounded" placeholder="Doe" required />
+                        </div>
+                    </div>
                     <div className="mb-4">
                         <label htmlFor="email" className="block mb-1 text-left text-gray-700">Email</label>
                         <input type="text" name="email" className="w-full p-2 border border-gray-300 rounded" placeholder="john.doe@company.com" required />
@@ -22,6 +33,15 @@ const SignupForm = () => {
                         <label htmlFor="bio" className="block mb-1 text-left text-gray-700">Bio</label>
                         <textarea name="bio" rows={4} className="block w-full text-sm p-2 border rounded-lg focus:ring-blue-500 border-gray-300" placeholder="Your bio here..." ></textarea>
                     </div>
+
+
+                    <label class="inline-flex items-center mb-5 cursor-pointer">
+                        <input type="checkbox" value="" class="sr-only peer" />
+                            <div class="relative w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
+                            <span class="ms-3 text-sm font-medium text-gray-700">I am mentor</span>
+                    </label>
+
+
                     <div className="mb-4">
                         <button className="w-3xs bg-blue-900 text-white py-2 rounded" style={{
                             background: 'linear-gradient(to bottom, #1e3a8a, #384A68)',
@@ -29,7 +49,7 @@ const SignupForm = () => {
                     </div>
                     <div>
                         <p className="text-gray-600">
-                            Do you have an account? <Link to='/Login' className="text-red-700 font-semibold">Sign in</Link>
+                            Do you have an account? <Link to='/Login' className="text-red-700">Sign in</Link>
                         </p>
                     </div>
                 </div>
