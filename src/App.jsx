dc8f206fdbf74b6cb64291f954775b66d59acef3
Leg Aman <126026416+leg-aman@users.noreply.github.com>
2025-09-03 19:35:49 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-// import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-// import { getAllData } from "./util/index";
 import NoPage from "./pages/NoPage";
 import SignUp from "./features/auth/pages/SignupPage";
 import Login from "./features/auth/pages/LoginPage";
-// const URL = "http://localhost:8000/api/v1/";
 import { AuthProvider } from "./context/AuthContext";
 import Router from "./router/Router";
 import "./index.css";
-
 function App() {
   return (
     <>
