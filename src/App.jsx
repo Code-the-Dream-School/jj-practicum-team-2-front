24c5f896a9b45e69bab02d5e3a8d213fc6b9fb0c
Leg Aman <126026416+leg-aman@users.noreply.github.com>
2025-09-04 17:28:17 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,9 @@ import Router from "./router/Router";
 import "./index.css";
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="login" element={<Login />} />
-        <Route path="*" element={<NoPage />} />
-      </Routes>
-
-      <div className="min-h-screen flex items-center justify-center bg-gray-100">
-        <div className="text-center">
-          <h1 className="text-4xl font-bold text-slate-800 mb-4">MentorHub</h1>
-          <p className="text-lg text-slate-600">
-            Welcome to MentorHub! Your gateway to connecting with mentors and
-            advancing your career.
-          </p>
-        </div>
-      </div>
-
       <AuthProvider>
         <Router />
       </AuthProvider>
-    </>
   );
 }
 
