49a97e96c8f40c4de97e61d9af460587315dada8
leg-aman <126026416+leg-aman@users.noreply.github.com>
2025-09-05 22:20:56 -0400

    update on connecting API

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,12 +1,12 @@
 import WelcomeSidebar from "../components/common/WelcomeSidebar";
-import SignupForm from "../components/auth/RegisterForm";
+import RegisterForm from "../components/auth/RegisterForm";
 export default function RegisterPage() {
   return (
     <div className="flex w-full h-screen flex-wrap lg:flex-nowrap text-center">
       {/* left side */}
       <WelcomeSidebar />
       {/* right side */}
-      <SignupForm />
+      <RegisterForm />
     </div>
   );
 }
